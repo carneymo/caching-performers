2f7f11655aa994eec7646121476b04385eced8f6
Ryan CM <ryan.mogan@proton.me>
2024-03-24 12:35:36 -0600

    Kafka integration plus client/server scripts

--- shutdown.sh
+++ shutdown.sh
@@ -1,3 +1,4 @@
 docker-compose -f ./garnet/docker-compose.yml down
 docker-compose -f ./redis/docker-compose.yml down
 docker-compose -f ./dragonfly/docker-compose.yml down
+docker-compose -f ./kafka/docker-compose.yml down
--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 docker-compose -f ./garnet/docker-compose.yml up -d 
 docker-compose -f ./redis/docker-compose.yml up -d 
 docker-compose -f ./dragonfly/docker-compose.yml up -d 
+docker-compose -f ./kafka/docker-compose.yml up -d 
